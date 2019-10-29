1f8fc2508209d0b275f8e2e5dfd277a9f8bff58c
ladyada <limor@ladyada.net>
2019-10-28 21:34:21 -0400

    demo with display output

---
+++ examples/hello_world_arcada/arduino_output_handler.cpp
@@ -0,0 +1,68 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "output_handler.h"
+#include "Arduino.h"
+#include "Adafruit_Arcada.h"
+Adafruit_Arcada arcada;
+
+// The pin of the Arduino's built-in LED
+int led = LED_BUILTIN;
+
+// Track whether the function has run at least once
+bool initialized = false;
+
+// helper function to let us scale floating point values nicely
+double mapf(double x, double in_min, double in_max, double out_min, double out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+// Animates a dot across the screen to represent the current x and y values
+void HandleOutput(tflite::ErrorReporter* error_reporter, float x_value,
+                  float y_value) {
+  // Do this only once
+  if (!initialized) {
+    // Set the display to be on!    
+    arcada.arcadaBegin();
+    arcada.displayBegin();
+    arcada.setBacklight(255);
+    // Add some text to describe what's up!
+    arcada.display->fillScreen(ARCADA_BLACK);
+    arcada.display->setTextColor(ARCADA_WHITE);
+    const char *header = "TensorFlow Lite";
+    const char *footer = "Sine wave model";
+    arcada.display->setCursor((arcada.display->width()-strlen(header)*6)/2, 0);
+    arcada.display->print(header);
+    arcada.display->setCursor((arcada.display->width()-strlen(footer)*6)/2, arcada.display->height()-8);
+    arcada.display->print(footer);
+    initialized = true;
+  }
+
+  // map the x input value (0-2*PI) and the y value (-1.5 to 1.5)
+  // to the size of the display
+  float pixel_x, pixel_y;
+  static float last_pixel_x, last_pixel_y;
+  pixel_x = mapf(x_value, 0, 2*3.1415, 0, arcada.display->width());
+  pixel_y = mapf(y_value, -1.75, 1.75, 0, arcada.display->height());
+
+  // arcada.display->drawPixel(pixel_x, pixel_y, ST77XX_GREEN);
+  arcada.display->fillCircle(last_pixel_x, last_pixel_y, 5, ARCADA_BLACK);
+  arcada.display->fillCircle(pixel_x, pixel_y, 5, ST77XX_RED);
+  last_pixel_x = pixel_x;
+  last_pixel_y = pixel_y;
+
+  // slow it down so we can see the ball!
+  delay(3);
+}
---
+++ examples/hello_world_arcada/hello_world_arcada.ino
@@ -0,0 +1,88 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <TensorFlowLite.h>
+#include "Adafruit_TFLite.h"
+
+#include "output_handler.h"
+#include "sine_model_data.h"
+
+// Create an area of memory to use for input, output, and intermediate arrays.
+// Finding the minimum value for your model may require some trial and error.
+const int kTensorAreaSize  (2 * 1024);
+
+// This constant represents the range of x values our model was trained on,
+// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
+// libraries.
+const float kXrange = 2.f * 3.14159265359f;
+
+// Will need tuning for your chipset
+const int kInferencesPerCycle = 200;
+int inference_count = 0;
+
+Adafruit_TFLite ada_tflite(kTensorAreaSize);
+
+// The name of this function is important for Arduino compatibility.
+void setup() {
+  Serial.begin(115200);
+  //while (!Serial) yield();
+  
+  if (! ada_tflite.begin()) {
+    Serial.println("Failed to initialize TFLite");
+    while (1) yield();
+  }
+
+  if (! ada_tflite.loadModel(g_sine_model_data)) {
+    Serial.println("Failed to load model");
+    while (1) yield();
+  }
+
+  // Keep track of how many inferences we have performed.
+  inference_count = 0;
+}
+
+// The name of this function is important for Arduino compatibility.
+void loop() {
+  // Calculate an x value to feed into the model. We compare the current
+  // inference_count to the number of inferences per cycle to determine
+  // our position within the range of possible x values the model was
+  // trained on, and use this to calculate a value.
+  float position = static_cast<float>(inference_count) /
+                   static_cast<float>(kInferencesPerCycle);
+  float x_val = position * kXrange;
+
+  // Place our calculated x value in the model's input tensor
+  ada_tflite.input->data.f[0] = x_val;
+
+  // Run inference, and report any error
+  TfLiteStatus invoke_status = ada_tflite.interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    ada_tflite.error_reporter->Report("Invoke failed on x_val: %f\n",
+                           static_cast<double>(x_val));
+    return;
+  }
+
+  // Read the predicted y value from the model's output tensor
+  float y_val = ada_tflite.output->data.f[0];
+
+  // Output the results. A custom HandleOutput function can be implemented
+  // for each supported hardware target.
+  HandleOutput(ada_tflite.error_reporter, x_val, y_val);
+
+  // Increment the inference_counter, and reset it if we have reached
+  // the total number per cycle
+  inference_count += 1;
+  if (inference_count >= kInferencesPerCycle) inference_count = 0;
+}
