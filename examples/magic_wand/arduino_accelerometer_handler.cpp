491dc20094a38d53f298e8115f8aff0b18876790
ladyada <limor@ladyada.net>
2019-10-30 03:43:18 -0400

    neopixel demo for pybadge, circuit playground bluefruit

---
+++ examples/magic_wand/arduino_accelerometer_handler.cpp
@@ -0,0 +1,193 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "accelerometer_handler.h"
+
+#include <Arduino.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+
+/* this is a little annoying to figure out, as a tip - when
+ * holding the board straight, output should be (0, 0, 1)
+ * tiling the board 90* left, output should be (0, 1, 0)
+ * tilting the board 90* forward, output should be (1, 0, 0);
+ */
+ 
+#if defined(ADAFRUIT_PYBADGE_M4_EXPRESS)
+// holding up with screen/neopixels facing you
+const int X_POSITION = 1;
+const int Y_POSITION = 2;
+const int Z_POSITION = 0;
+const bool INVERT_X = true;
+const bool INVERT_Y = true;
+const bool INVERT_Z = false;
+Adafruit_LIS3DH accel = Adafruit_LIS3DH();
+#define NUM_PIXELS  5
+#define PIXELS_PIN  8
+#endif
+
+#if defined(ARDUINO_NRF52840_CIRCUITPLAY)
+// holding up with neopixels facing you
+const int X_POSITION = 1;
+const int Y_POSITION = 2;
+const int Z_POSITION = 0;
+const bool INVERT_X = false;
+const bool INVERT_Y = false;
+const bool INVERT_Z = false;
+Adafruit_LIS3DH accel = Adafruit_LIS3DH(&Wire1);
+#define NUM_PIXELS  10
+#define PIXELS_PIN  8
+#endif
+
+Adafruit_NeoPixel pixels(NUM_PIXELS, PIXELS_PIN, NEO_GRB + NEO_KHZ800);
+
+
+#include "constants.h"
+
+// A buffer holding the last 200 sets of 3-channel values
+float save_data[600] = {0.0};
+// Most recent position in the save_data buffer
+int begin_index = 0;
+// True if there is not yet enough data to run inference
+bool pending_initial_data = true;
+// How often we should save a measurement during downsampling
+int sample_every_n;
+// The number of measurements since we last saved one
+int sample_skip_counter = 1;
+
+uint32_t last_reading_stamp = 0;
+
+TfLiteStatus SetupAccelerometer(tflite::ErrorReporter* error_reporter) {
+  // Wait until we know the serial port is ready
+  //while (!Serial) { yield(); }
+
+  pixels.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.show();            // Turn OFF all pixels ASAP
+  pixels.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  // Switch on the IMU
+  if (!accel.begin(0x18) && !accel.begin(0x19)) {
+    error_reporter->Report("Failed to initialize IMU");
+    pixels.fill(pixels.Color(50, 0, 0));
+    pixels.show();
+    return kTfLiteError;
+  }
+
+  accel.setRange(LIS3DH_RANGE_4_G);
+  accel.setDataRate(LIS3DH_DATARATE_25_HZ);
+  float sample_rate = 25;
+  
+
+  // Determine how many measurements to keep in order to
+  // meet kTargetHz
+  sample_every_n = static_cast<int>(roundf(sample_rate / kTargetHz));
+
+  error_reporter->Report("Magic starts!");
+
+  return kTfLiteOk;
+}
+
+bool ReadAccelerometer(tflite::ErrorReporter* error_reporter, float* input,
+                       int length, bool reset_buffer) {
+  // Clear the buffer if required, e.g. after a successful prediction
+  if (reset_buffer) {
+    memset(save_data, 0, 600 * sizeof(float));
+    begin_index = 0;
+    pending_initial_data = true;
+  }
+  // Keep track of whether we stored any new data
+  bool new_data = false;
+  // Loop through new samples and add to buffer
+  while (accel.haveNewData()) {
+    float x, y, z;
+    
+    // Read each sample, removing it from the device's FIFO buffer
+    sensors_event_t event; 
+    
+    if (! accel.getEvent(&event)) {
+      error_reporter->Report("Failed to read data");
+      break;
+    }
+    
+    // Throw away this sample unless it's the nth
+    if (sample_skip_counter != sample_every_n) {
+      sample_skip_counter += 1;
+      continue;
+    }
+    
+    float values[3] = {0, 0, 0};
+    values[X_POSITION] = event.acceleration.x / 9.8;
+    values[Y_POSITION] = event.acceleration.y / 9.8;
+    values[Z_POSITION] = event.acceleration.z / 9.8;
+
+    x = values[0];
+    y = values[1];
+    z = values[2];
+
+    if (INVERT_X) {
+      x *= -1;
+    }
+    if (INVERT_Y) {
+      y *= -1;
+    }
+    if (INVERT_Z) {
+      z *= -1;
+    }
+    Serial.print(x, 2);
+    Serial.print(", "); Serial.print(y, 2);
+    Serial.print(", "); Serial.println(z, 2);
+    
+    last_reading_stamp = millis();
+    // Write samples to our buffer, converting to milli-Gs
+    save_data[begin_index++] = x * 1000;
+    save_data[begin_index++] = y * 1000;
+    save_data[begin_index++] = z * 1000;
+    
+    // Since we took a sample, reset the skip counter
+    sample_skip_counter = 1;
+    // If we reached the end of the circle buffer, reset
+    if (begin_index >= 600) {
+      begin_index = 0;
+    }
+    new_data = true;
+  }
+
+  // Skip this round if data is not ready yet
+  if (!new_data) {
+    return false;
+  }
+
+  // Check if we are ready for prediction or still pending more initial data
+  if (pending_initial_data && begin_index >= 200) {
+    pending_initial_data = false;
+  }
+
+  // Return if we don't have enough data
+  if (pending_initial_data) {
+    return false;
+  }
+
+  // Copy the requested number of bytes to the provided input tensor
+  for (int i = 0; i < length; ++i) {
+    int ring_array_index = begin_index + i - length;
+    if (ring_array_index < 0) {
+      ring_array_index += 600;
+    }
+    input[i] = save_data[ring_array_index];
+  }
+
+  return true;
+}
